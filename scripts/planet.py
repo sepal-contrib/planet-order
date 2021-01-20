1b70c9edd111bfd90bf0d613f4243ea44f859c7e
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-01-20 07:43:40 +0000

    create a theorical planet grid

--- scripts/planet.py
+++ scripts/planet.py
@@ -8,17 +8,18 @@ import time
 
 import geopandas as gpd
 from shapely import geometry as sg
-from pandas import json_normalize
+import pandas as pd
 import geemap
 import ipyvuetify as v
+import numpy as np
+import ee 
+
+ee.Initialize()
 
 from . import parameter as pm
 
 def paginate(session, url):
-    page_url = url
-    page = session.get(page_url).json()
-    
-    return page
+    return session.get(url).json()
 
 def get_scenes_per_quad(quad, session):
     url = quad["_links"]["items"]
@@ -108,30 +109,17 @@ def get_orders(planet_api_key, basemaps_url, output):
     # Getting mosaics metadata
     orders = response.json()["mosaics"]
     if len(orders) == 0:
-        output.add_msg(get_error("e1"), 'error')
-        return
+        raise Excception(get_error("e1"))
         
     return orders, session
 
-def get_grid(planet_api_key, basemaps_url, aoi_io, m, output):
+def get_grid(quads, aoi_io, m, output):
     
-    # get the orders 
-    orders, session = get_orders(planet_api_key, basemaps_url, output)
-    
-    # select the first order
-    mosaic_df = json_normalize(orders)
-    mosaic_url = basemaps_url + mosaic_df.iloc[0].id
-
-    # Getting the quads
-    quads_url = mosaic_url + '/quads'
-    payload = {"_page_size": 1000, 'bbox': ', '.join(str(x) for x in aoi_io.get_bounds(aoi_io.get_aoi_ee()))}
-    quads = get_quads(quads_url, payload, session)
-    
-    df = json_normalize(quads)
+    df = pd.json_normalize(quads)
     geometry = [sg.box(*row.bbox) for i, row in df.iterrows()]
     gdf = gpd.GeoDataFrame(df.filter(['id']), geometry=geometry, crs="EPSG:4326")
     
-    grid_path = pm.get_result_dir(aoi_io.get_aoi_name()).joinpath(f'planet_grid.shp')
+    grid_path = pm.get_result_dir(aoi_io.get_aoi_name()).joinpath(f'{aoi_io.get_aoi_name()}_planet_grid.shp')
     gdf.to_file(grid_path)
     
     # display on map 
@@ -145,14 +133,16 @@ def get_grid(planet_api_key, basemaps_url, aoi_io, m, output):
     
     return grid_path
     
-def run_download(planet_api_key, basemaps_url, aoi_io, order_index, output):
+def run_download(planet_api_key, basemaps_url, aoi_io, order_io, m, output):
     
-    orders, session = get_orders(planet_api_key, basemaps_url, output)
+    # get the orders 
+    orders = order_io.orders
+    session = order_io.session
     
     # maybe insert number as a variable in the interface    
-    mosaic_name = orders[order_index]["name"]
+    mosaic_name = orders[order_io.order_index]["name"]
     
-    mosaics_df = json_normalize(orders)
+    mosaics_df = pd.json_normalize(orders)
 
     if mosaic_name in mosaics_df.name.values:
         mosaic_id = mosaics_df.loc[mosaics_df["name"] == mosaic_name]["id"].values
@@ -165,13 +155,16 @@ def run_download(planet_api_key, basemaps_url, aoi_io, order_index, output):
         quads = get_quads(quads_url, payload, session)
 
         if isinstance(quads, list):
+            
+            grid_path = get_grid(quads, aoi_io, m, output)
+            
             output.add_msg(f"Preparing the download of {len(quads)} quads for mosaic {mosaic_name}")
             mosaic_path = download_quads(quads, mosaic_name, session, aoi_io.get_aoi_name(), output)
             
         else:
-            output.add_msg(get_error("e4", quads=quads), 'error')
+            raise Exception(get_error("e4", quads=quads))
     
-    return mosaic_path
+    return (mosaic_path, grid_path)
     
 def get_sum_up(aoi_io):
     
@@ -185,3 +178,61 @@ def get_sum_up(aoi_io):
     msg = f"You're about to launch a downloading on a surface of {surface} Km\u00B2"
     
     return msg
+
+def get_theorical_grid(aoi_io, m, output):
+    """get the theorical grid on the selected aoi"""
+    
+    # get the aoi as a shapefile 
+    aoi_json = geemap.ee_to_geojson(aoi_io.get_aoi_ee())
+    gdf = gpd.GeoDataFrame.from_features(aoi_json).set_crs('EPSG:4326').to_crs('EPSG:3857')
+        
+    min_lon_aoi, min_lat_aoi, max_lon_aoi, max_lat_aoi = gdf.total_bounds
+    
+    # bounds of the world in 3857
+    min_lon = -20026376.39 
+    max_lon = 20026376.39
+    min_lat = -20048966.10
+    max_lat = 20048966.10
+
+    # size of each cell for zoom 15
+    size = 4.77 * 4096
+
+    # compute latitude and longitude 
+    longitudes = np.arange(min_lon, max_lon, size)
+    latitudes = np.arange(min_lat, max_lat, size)
+
+    # create the grid geometry data  
+    data = {'id':[],'geometry':[]}
+    for i, x in enumerate(longitudes):
+        for j, y in enumerate(latitudes):
+        
+            # identify if the point is inside the total bounds 
+            inside_lon = (x + size > min_lon_aoi) and (x < max_lon_aoi)
+            inside_lat = (y + size > min_lat_aoi) and (y < max_lat_aoi)
+            
+            # add the geometry
+            if inside_lon and inside_lat:
+                data['id'].append(f'{i}-{j}')
+                data['geometry'].append(sg.box(x, y, x + size, y + size))
+                        
+    
+    # create and clip the grid over the country geometry
+    grid = gpd.GeoDataFrame(data, crs='EPSG:3857')  
+    grid = gpd.clip(grid, gdf)
+    grid = grid.to_crs('EPSG:4326')
+            
+    # save the file
+    grid_path = pm.get_result_dir(aoi_io.get_aoi_name()).joinpath(f'{aoi_io.get_aoi_name()}_grid.shp')
+    grid.to_file(grid_path)
+            
+    # display on map 
+    json_df = json.loads(grid.to_json())
+    ee_df = geemap.geojson_to_ee(json_df)
+    
+    m.addLayer(ee_df, {'color': v.theme.themes.dark.warning}, 'theorical grid')
+    m.zoom_ee_object(ee_df.geometry())
+    
+    output.add_live_msg(f'The grid have been created and is available at {grid_path}', 'success')
+            
+    return grid_path
+            
