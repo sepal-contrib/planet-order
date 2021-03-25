d6f322c7d8cedfac523ff84728da00a548c5daea
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-03-25 07:27:51 +0000

    read the planet.key file

--- component/parameter/__init__.py
+++ component/parameter/__init__.py
@@ -1,1 +1,2 @@
 from .directory import *
+from .planet import *
---
+++ component/parameter/planet.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+default_planet_key = None
+
+# if the planet_key file exist use a default_planet_key
+planet_key_file = Path(__file__).parents[2].joinpath('planet.key')
+if planet_key_file.is_file():
+    
+    with planet_key_file.open() as f:
+        default_planet_key = f.read().strip()
