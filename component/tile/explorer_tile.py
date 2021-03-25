b037b61ce80e8e43b83ea5e507c2e15ac381fcb8
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-03-25 15:41:34 +0000

    add the download btn

--- component/tile/explorer_tile.py
+++ component/tile/explorer_tile.py
@@ -15,6 +15,7 @@ class ExplorerTile(sw.Tile):
         # create the widgets 
         self.api_key = cw.CustomPassword()
         self.check_key = sw.Btn('check', block = True)
+        self.down_quads = sw.Btn('download', block = True, disabled = True, class_ = 'mt-5').hide()
         self.api_alert = sw.Alert()
         self.select = cw.DynamicSelect()
         self.m = cw.DownMap()
@@ -25,7 +26,7 @@ class ExplorerTile(sw.Tile):
             row = True,
             align_center = True,
             children = [
-                v.Flex(xs3=True, class_ = "ma-2", children = [txt, v.Divider(class_='mb-3'), self.api_key, self.check_key, self.api_alert]),
+                v.Flex(xs3=True, class_ = "ma-2", children = [txt, v.Divider(class_='mb-3'), self.api_key, self.check_key, self.down_quads, self.api_alert]),
                 v.Flex(children = [self.select, self.m])
             ]
         )
@@ -39,22 +40,36 @@ class ExplorerTile(sw.Tile):
         
         # add js behaviour
         self.check_key.on_event('click', self._get_mosaic)
+        self.select.observe(self._on_mosaic_select, 'v_model')
         
     def _get_mosaic(self, widget, event, data):
         """recover all the available mosaic with this specific key"""
         
         widget.toggle_loading()
         
-        #try: 
-        items = cs.order_basemaps(self.api_key.v_model, self.api_alert)
-        self.select.set_items(items)
+        try: 
+            items = cs.order_basemaps(self.api_key.v_model, self.api_alert)
+            self.select.set_items(items)
+            self.down_quads.show()
             
-        #except Exception as e:
-        #    self.api_alert.add_msg(str(e), 'error')
+        except Exception as e:
+            self.api_alert.add_msg(str(e), 'error')
             
         widget.toggle_loading()
         
         return self
+    
+    def _on_mosaic_select(self, change):
+        """load the mosaics on the map and release the download btn"""
+        
+        # unable the btn 
+        if change['new']:
+            self.down_quads.disabled = False
+            
+        # update the map 
+        
+        return self
+    
     
         
         
