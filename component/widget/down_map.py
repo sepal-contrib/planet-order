3cf5d74ce1602e600da43321d5a2e5124045d9ff
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-03-25 23:30:04 +0000

    improve map display

--- component/widget/down_map.py
+++ component/widget/down_map.py
@@ -10,19 +10,11 @@ class DownMap(sm.SepalMap):
     
     def __init__(self):
         
-        # create the extra widgets 
+        # create the extra widget
         self.state = sw.StateBar(done=True)
-        self.down_btn = v.Btn(
-            small = True,
-            children = [v.Icon(class_ = "ma-1", children=['mdi-download'])]
-        )
         
         # create the map 
         super().__init__()
         
         # add the widget to the map (as to left and right items)
-        controls = [
-            WidgetControl(widget=self.down_btn, position='topright', transparent_bg=True),
-            WidgetControl(widget=self.state, position='topleft')
-        ] + list(self.controls)
-        self.controls = tuple(controls)
+        self.add_control(WidgetControl(widget=self.state, position='topleft'))
