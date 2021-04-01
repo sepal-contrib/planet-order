4acb34b1d184163b650db2158f61485953f1d3c0
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-04-01 07:23:47 +0000

    build the planet grid on the fly Fix #12

--- component/scripts/mapping.py
+++ component/scripts/mapping.py
@@ -1,4 +1,5 @@
 import json 
+from itertools import product
 
 import ee
 from shapely import geometry as sg
@@ -7,6 +8,7 @@ import geopandas as gpd
 import ipyvuetify as v
 import geemap
 import numpy as np
+from pyproj import CRS, Transformer
 
 from component import parameter as cp
 from component.message import cm
@@ -32,7 +34,7 @@ def set_grid(aoi_io, m, out):
     
     out.add_msg(cm.map.grid)
     
-    # chec the grid filename 
+    # check the grid filename 
     aoi_name = aoi_io.get_aoi_name()
     grid_file = cp.get_aoi_dir(aoi_name).joinpath(f'{aoi_name}_grid.geojson')
     
@@ -43,52 +45,72 @@ def set_grid(aoi_io, m, out):
     # generate the grid
     else:
     
-        # get the shape of the aoi in EPSG:4326 proj 
+        # get the shape of the aoi in EPSG:3857 proj 
         aoi_json = geemap.ee_to_geojson(aoi_io.get_aoi_ee())
         aoi_shp = unary_union([sg.shape(feat['geometry']) for feat in aoi_json['features']])
-        aoi_gdf = gpd.GeoDataFrame({'geometry': [aoi_shp]}, crs="EPSG:4326")
+        aoi_gdf = gpd.GeoDataFrame({'geometry': [aoi_shp]}, crs="EPSG:4326").to_crs('EPSG:3857')
+        
+        # extract the aoi shape 
+        aoi_shp_proj = aoi_gdf['geometry'][0]
     
         # retreive the bb 
         aoi_bb = sg.box(*aoi_gdf.total_bounds)
-    
-        # create a dataframe that only use the grid cells that are intersecting the bb of the aoi_gdf
-        with cp.planet_grid.open() as f:
-    
-            features = []
         
-            # jump over the first line
-            next(f)
-        
-            # read each line of the file
-            for line in f:
-        
-                # split the line 
-                tmp = line.strip().split(sep=', ')
-        
-                # create a box object
-                bb = sg.box(*json.loads(tmp[3]))
-        
-                # build a feature if it intersects the aoi
-                if bb.intersects(aoi_bb):
-                    feat = {
-                        'type': 'Feature', 
-                        'properties': {
-                            'name': tmp[0], 
-                            'x': tmp[1], 
-                            'y': tmp[2]
-                        }, 
-                        'geometry': bb.__geo_interface__
-                    }
-            
-                    features.append(feat)
+        # compute the longitude and latitude in the apropriate CRS
+        crs_4326 = CRS.from_epsg(4326)
+        crs_3857 = CRS.from_epsg(3857)
+        crs_bounds = crs_3857.area_of_use.bounds
 
-        grid_gdf = gpd.GeoDataFrame.from_features(features, crs='EPSG:4326')
+        proj = Transformer.from_crs(4326, 3857, always_xy=True)
+        bl = proj.transform(crs_bounds[0], crs_bounds[1])
+        tr = proj.transform(crs_bounds[2], crs_bounds[3])
 
-        # filter by cliping 
-        grid_gdf.geometry = grid_gdf.intersection(aoi_shp)
+        # the planet grid is constructing a 2048x2048 grid of SQUARES. 
+        # The latitude extends is bigger (20048966.10m VS 20026376.39) so to ensure the "squariness"
+        # Planet lab have based the numerotation and extends of it square grid on the longitude only. 
+        # the extreme -90 and +90 band it thus exlucded but there are no forest there so we don't care
+        longitudes = np.linspace(bl[0], tr[0], 2048+1)
+
+        # the planet grid size cut the world in 248 squares vertically and horizontally
+        box_size = (tr[0]-bl[0])/2048
+
+        # filter with the geometry bounds
+        bb = aoi_gdf.total_bounds
+
+        # filter lon and lat 
+        lon_filter = longitudes[(longitudes > (bb[0] - box_size)) & (longitudes < bb[2] + box_size)]
+        lat_filter = longitudes[(longitudes > (bb[1] - box_size)) & (longitudes < bb[3] + box_size)]
+
+        # get the index offset 
+        x_offset = np.nonzero(longitudes == lon_filter[0])[0][0]
+        y_offset = np.nonzero(longitudes == lat_filter[0])[0][0]
     
-        # filter empty geometries
-        grid_gdf = grid_gdf[np.invert(grid_gdf.is_empty)]
+        # create the grid
+        x = []
+        y = []
+        names = []
+        squares = []
+        for coords in product(range(len(lon_filter)-1), range(len(lat_filter)-1)):
+    
+            # get the x and y index 
+            ix = coords[0]
+            iy = coords[1]
+        
+            # fill the grid values 
+            x.append(ix + x_offset)
+            y.append(iy + y_offset)
+            names.append(f'L15-{x[-1]:4d}E-{y[-1]:4d}N.tif')
+            squares.append(sg.box(lon_filter[ix], lat_filter[iy], lon_filter[ix+1], lat_filter[iy+1]))
+        
+        # create a buffer grid in lat-long
+        grid = gpd.GeoDataFrame({'x':x, 'y':y, 'names':names, 'geometry':squares}, crs='EPSG:3857')
+
+        # cut the grid to the aoi extends 
+        mask = grid.intersects(aoi_shp_proj)
+        grid = grid.loc[mask]
+    
+        # project back to 4326
+        grid_gdf = grid.to_crs('EPSG:4326')
     
         # save the grid
         grid_gdf.to_file(grid_file, driver='GeoJSON')
@@ -100,11 +122,6 @@ def set_grid(aoi_io, m, out):
     # display the grid on the map
     empty = ee.Image().byte()
     outline = empty.paint(featureCollection = grid_ee, color = 1, width = 3)
-    m.addLayer(outline, {'palette': v.theme.themes.dark.accent}, 'AOI Planet© Grid', False)
+    m.addLayer(outline, {'palette': v.theme.themes.dark.primary}, 'AOI Planet© Grid', True)
     
-    return grid_gdf
-    
-    
-    
-    
-    
+    return grid_gdf 
