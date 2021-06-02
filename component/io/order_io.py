bd8b9dbee1a10da28c7c803b6b6692a9a1aef8ce
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-06-02 10:05:39 +0000

    adapt to sepal_ui v2

--- component/io/order_io.py
+++
@@ -1,7 +0,0 @@
-class OrderIo:
-    
-    def __init__(self):
-        self.order_index = None
-        self.orders = None
-        self.session = None
-        self.quads = None
---
+++ component/model/order_model.py
@@ -0,0 +1,9 @@
+from sepal_ui import model 
+from traitlets import Any
+
+class OrderModel (model.Model):
+
+    order_index = Any(None).tag(sync=True)
+    orders = Any(None).tag(sync=True)
+    session = Any(None).tag(sync=True)
+    quads = Any(None).tag(sync=True)
